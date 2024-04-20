c402e4d5a2dd9d5f301c87c58858246d36287b68
Rishabh Kumar <rk9698184@gmail.com>
2024-04-20 19:26:22 +0530

    Java Code

---
+++ SimpleDialog2a.java
@@ -0,0 +1,11 @@
+
+
+import javax.swing.JOptionPane;
+
+public class SimpleDialog2a {
+
+    public static void main(String[] args){
+        JOptionPane.showMessageDialog(null, 55.7, "This is not an integer.", JOptionPane.PLAIN_MESSAGE);
+    }
+
+}
